872533d09437af1e4eb6ad9bb02007d877d2a37d
aly511 <alyxmomdouh@gmail.com>
2021-11-13 17:51:58 +0200

    add Builder design pattern

---
+++ 1-CreationalDesignPatterns/3-BuilderDesignPattern/Builder/Program.cs
@@ -0,0 +1,41 @@
+﻿using Builder.entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Program
+    {
+        static void WriteColoredLine( string text, ConsoleColor color = ConsoleColor.Green)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+        }
+        static void Main(string[] args)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            sb.Append("Word 1,");
+            sb.Append("Word 2"); 
+            WriteColoredLine(sb.ToString(), ConsoleColor.Cyan);
+            //////////////
+             WriteColoredLine("***Builder Pattern***",ConsoleColor.Yellow);
+            Director director = new Director();
+            IBuilder carBuilder = new Car("Jeep");
+            IBuilder motorCycleBuilder = new MotorCycle("Honda"); 
+
+            // Making Car
+             director.Construct(carBuilder);
+            Product car = carBuilder.GetVehicle();
+            WriteColoredLine($"Car {car.Show()}");
+            
+            //Making MotorCycle
+            director.Construct(motorCycleBuilder);
+            Product motorCycle = motorCycleBuilder.GetVehicle();
+            WriteColoredLine($"MotorCycle {motorCycle.Show()}"); 
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1-CreationalDesignPatterns/3-BuilderDesignPattern/Builder/entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder.entities
+{
+    public class Product  // or Builder
+    {// collect all parts  very thing is a product
+        private LinkedList<string> parts;
+        public Product() { parts = new LinkedList<string>(); }
+        public void Add(string part)
+        {
+            //Add parts
+            parts.AddLast(part);
+        }
+        public string Show()
+        {
+             StringBuilder result = new StringBuilder();
+            result.AppendLine("Product components are :");
+            foreach (string part in parts)
+                result.AppendLine(part);
+
+            return result.ToString();
+        }
+    }
+}
